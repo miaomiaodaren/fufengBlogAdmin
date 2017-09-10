f3db5a6148a1ca241cd2edb3d2cb774ef1b74497
chuanshanjia <chanshanjia@lljz-inc.com>
2017-09-10 20:13:49 +0800

    11

--- controller/user.js
+++ controller/user.js
@@ -8,7 +8,7 @@ class Users{
     async getweixintoken(req, res, next) {
 	    if(req.query.code) {
 	        const weixintoken = await superagent.get('https://api.weixin.qq.com/sns/oauth2/access_token').query('appid=wxe5dfd16048e8d9ae&secret=5519c4e7a5913c015971cff9359d5d61&code='+ req.query.code +'&grant_type=authorization_code');
-	        const usertoken = JSON.parse(cnnodedata.text);
+	        const usertoken = JSON.parse(weixintoken.text);
 	        const userinfo = await superagent.get('https://api.weixin.qq.com/sns/userinfo').query('access_token='+ usertoken.access_token +'openid=' + usertoken.openid + 'lang=zh_CN');
 	        console.info(userinfo);
 	        res.render('weixin', {title: 'woshifufeng', userinfos: userinfo});
